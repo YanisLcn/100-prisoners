f855c681460de747ad1da855c4b1597ecc8d96c7
GitFenixZ <soantonyly@gmail.com>
2023-12-18 07:26:04 +0100

    feat: add images

--- tools/strategies_comparison.py
+++ tools/strategies_comparison.py
@@ -134,10 +134,22 @@ def compare_strategies_outcomes(random_prisonners_record, optimal_prisonners_rec
         align="center",
     )
 
+    SMALL_SIZE = 8
+    MEDIUM_SIZE = 10
+    BIGGER_SIZE = 12
+
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
     # Ajout des détails du diagramme
     plt.xlabel("Nombre de prisonniers qui ont trouvé leur numéro")
     plt.ylabel("Probabilité")
-    plt.title("Probabilité que n prisonnier(s) aient trouvés leur numéro")
+    plt.title("Probabilité que n prisonniers aient trouvés leur numéro")
     plt.legend(title="Stratégie")
     plt.gca().yaxis.set_major_formatter(PercentFormatter(nb_of_iterations))
     plt.xticks(np.arange(0, 101, step=5))
